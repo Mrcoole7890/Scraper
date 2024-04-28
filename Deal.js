90dc30c807cec0c74f0ee5ed1a5de4dffc7c4547
Cole A. Kendall <kendallcole7@gmail.com>
2024-04-28 14:35:36 -0400

    create mock for Deal object

---
+++ Deal.js
@@ -0,0 +1,20 @@
+class Deal {
+
+    constructor() {
+        this.fields = [
+            ["Name", "Kobalt - Blue Black Polyester 13.5-in Backpack"],
+            ["MSRP", "$59.98"],
+            ["Min Price", "$0.02"],
+            ["Percent Off", "100%"],
+            ["SKU", "5013497799"],
+            ["In-Store Item Number", "5064410"],
+            ["Barcode", "N/A"],
+            ["Sold Listings", "N/A"],
+            ["Brickseek", "N/A"],
+            ["Amazon Products", "N/A"],
+            ["Store Info", "N/A"]
+        ];
+    }
+}
+
+exports.Deal = Deal;
