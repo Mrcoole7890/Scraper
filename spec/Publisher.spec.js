df56fc62abdf8e21469c40a33364ebc062d8406c
Cole A. Kendall <kendallcole7@gmail.com>
2024-04-28 14:37:04 -0400

    add TCs

--- spec/Publisher.spec.js
+++ spec/Publisher.spec.js
@@ -4,6 +4,22 @@ const dotenv = require('dotenv').config();
 describe("Testing suite for the publisher file", function() {
     it("can be called in the testing suite", function() {
         var myPublisher = new Publishers.Publisher(process.env.WEBHOOK_FOR_PUBLISH);
-        expect(myPublisher).not.toBe(null);
+        expect(myPublisher).not.toBe(undefined);
+    });
+    it("will not set the message if the parameter is null", function() {
+        var myPublisher = new Publishers.Publisher(process.env.WEBHOOK_FOR_PUBLISH);
+        myPublisher.setMessage(null);
+        expect(myPublisher.getMessage()).toBe(null);
+    });
+    it("will not set the message if the parameter is an array but the elements are not all arrays of size 2", function() {
+        var myPublisher = new Publishers.Publisher(process.env.WEBHOOK_FOR_PUBLISH);
+        myPublisher.setMessage([1,3,5]);
+        expect(myPublisher.getMessage()).toBe(null);
+
+        myPublisher.setMessage( [[1,3,5], [1,5]] );
+        expect(myPublisher.getMessage()).toBe(null);
+
+        myPublisher.setMessage( [ [1,6] , [7,7,3] ] );
+        expect(myPublisher.getMessage()).toBe(null);
     });
 });
